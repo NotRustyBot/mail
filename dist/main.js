20ae90734c97347bc36c3a37ab2f46e196f949ac
Andrej Karovin <andrej.karovin2000@gmail.com>
2024-09-19 19:39:15 +0200

    seen fix

--- dist/main.js
+++ dist/main.js
@@ -3,49 +3,52 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.cleanMunipolis = void 0;
 const imapflow_1 = require("imapflow");
 const mailparser_1 = require("mailparser");
-const client = new imapflow_1.ImapFlow({
-    auth: {
-        user: "jacekmunipolis@email.cz",
-        pass: process.env.password,
-    },
-    host: "imap.seznam.cz",
-    port: 993,
-    logger: false,
-    logRaw: false,
-});
 const main = async () => {
-    // Wait until client connects and authorizes
+    const client = new imapflow_1.ImapFlow({
+        auth: {
+            user: "jacekmunipolis@email.cz",
+            pass: process.env.password,
+        },
+        host: "imap.seznam.cz",
+        port: 993,
+        logger: false,
+        logRaw: false,
+    });
     await client.connect();
-    await processBox("INBOX");
-    await processBox("newsletters");
+    await processBox("INBOX", client);
+    await processBox("newsletters", client);
     await client.logout();
 };
-async function processBox(boxName) {
+async function processBox(boxName, client) {
     let lock = await client.getMailboxLock(boxName);
     try {
+        let messages = new Array();
         for await (let message of client.fetch({ seen: false }, { source: true })) {
-            (0, mailparser_1.simpleParser)(message.source).then((mail) => {
-                let toSend = mail.text;
-                if (mail.from.value.find(e => e.address == "info@munipolis.cz"))
-                    toSend = cleanMunipolis(toSend);
-                console.log(toSend);
-                fetch("https://discord.com/api/webhooks/1205190222539919360/y76JGOjk72DFuv1CSzmEtyWdbyprlY-sNNGsE7wSa6FtLhLGGF8QWrPn6g5UkhDOUYX7", {
-                    method: "post",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        content: `**${mail.subject}**\n---\n${toSend}`.substring(0, 2000),
-                    }),
-                });
+            let mail = await (0, mailparser_1.simpleParser)(message.source);
+            messages.push(message.uid);
+            let toSend = mail.text;
+            if (mail.from.value.find((e) => e.address == "info@munipolis.cz"))
+                toSend = cleanMunipolis(toSend);
+            fetch("https://discord.com/api/webhooks/1205190222539919360/y76JGOjk72DFuv1CSzmEtyWdbyprlY-sNNGsE7wSa6FtLhLGGF8QWrPn6g5UkhDOUYX7", {
+                method: "post",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    content: `**${mail.subject}**\n---\n${toSend}`.substring(0, 2000),
+                }),
             });
         }
+        await client.messageFlagsSet(messages.join(","), ["\\Seen"]);
     }
     catch (err) {
         console.log(err);
     }
     lock.release();
 }
+setInterval(() => {
+    main().catch((err) => console.error(err));
+}, 1000 * 60);
 main().catch((err) => console.error(err));
 function cleanMunipolis(text) {
     text = text.split("-\n\nNastavení upozornění")[0];
--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,47 @@
 import { ImapFlow } from "imapflow";
 import { simpleParser } from "mailparser";
 
-const client = new ImapFlow({
-    auth: {
-        user: "jacekmunipolis@email.cz",
-        pass: process.env.password,
-    },
-    host: "imap.seznam.cz",
-    port: 993,
-    logger: false,
-    logRaw: false,
-});
+
 
 const main = async () => {
-    // Wait until client connects and authorizes
+    const client = new ImapFlow({
+        auth: {
+            user: "jacekmunipolis@email.cz",
+            pass: process.env.password,
+        },
+        host: "imap.seznam.cz",
+        port: 993,
+        logger: false,
+        logRaw: false,
+    });
+    
     await client.connect();
 
-    await processBox("INBOX");
-    await processBox("newsletters");
+    await processBox("INBOX", client);
+    await processBox("newsletters", client);
 
     await client.logout();
 };
 
-async function processBox(boxName: string) {
+async function processBox(boxName: string, client: ImapFlow) {
     let lock = await client.getMailboxLock(boxName);
     try {
         for await (let message of client.fetch({ seen: false }, { source: true })) {
-            simpleParser(message.source).then((mail) => {
-                let toSend = mail.text;
-                if (mail.from.value.find(e => e.address == "info@munipolis.cz")) toSend = cleanMunipolis(toSend);
-                console.log(toSend);
-                
-                fetch("https://discord.com/api/webhooks/1205190222539919360/y76JGOjk72DFuv1CSzmEtyWdbyprlY-sNNGsE7wSa6FtLhLGGF8QWrPn6g5UkhDOUYX7", {
-                    method: "post",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        content: `**${mail.subject}**\n---\n${toSend}`.substring(0, 2000),
-                    }),
-                })
+            let mail = await simpleParser(message.source);
+            let toSend = mail.text;
+            if (mail.from.value.find((e) => e.address == "info@munipolis.cz")) toSend = cleanMunipolis(toSend);
+
+            fetch("https://discord.com/api/webhooks/1205190222539919360/y76JGOjk72DFuv1CSzmEtyWdbyprlY-sNNGsE7wSa6FtLhLGGF8QWrPn6g5UkhDOUYX7", {
+                method: "post",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    content: `**${mail.subject}**\n---\n${toSend}`.substring(0, 2000),
+                }),
             });
         }
+        await client.messageFlagsSet({ seen: false }, ["\\Seen"]);
     } catch (err) {
         console.log(err);
     }
@@ -52,7 +52,7 @@ async function processBox(boxName: string) {
 setInterval(() => {
     main().catch((err) => console.error(err));
 }, 1000 * 60);
-
+main().catch((err) => console.error(err));
 
 export function cleanMunipolis(text: string) {
     text = text.split("-\n\nNastavení upozornění")[0];
