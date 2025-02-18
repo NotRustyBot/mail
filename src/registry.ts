6d9746083437be5f5f2b64f6ead8b9eef6b29ff7
Andrej Karovin <andrej.karovin2000@gmail.com>
2025-02-18 19:25:44 +0100

    update registry

--- src/registry.ts
+++ src/registry.ts
@@ -1,11 +1,16 @@
 import { Publisher } from "./publisher";
 import EmailPublisher from "./publishers/emailPublisher";
+import { GithubIssueCounterPublisher } from "./publishers/githubIssueCounterPublisher";
+import { WebhookPublisher } from "./publishers/webhookPublisher";
 import { Subscriber } from "./subscriber";
 import { DiscordWebhook } from "./subscribers/discordWebhook";
+import { EmailSubscriber } from "./subscribers/emailSubscriber";
 
 export function registerFactories() {
     Publisher.factoryLookup["EmailPublisher"] = EmailPublisher;
-
+    Publisher.factoryLookup["GithubIssueCounterPublisher"] = GithubIssueCounterPublisher;
+    Publisher.factoryLookup["WebhookPublisher"] = WebhookPublisher;
 
     Subscriber.factoryLookup["DiscordWebhook"] = DiscordWebhook;
+    Subscriber.factoryLookup["EmailSubscriber"] = EmailSubscriber;
 }
